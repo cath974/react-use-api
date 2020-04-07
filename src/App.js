7b758e00c41ab1802242d33c4e697753bf506bcb
Catherine <catblecon@gmail.com>
2020-04-07 07:41:11 +0100

    correction axios

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import './App.css';
 import axios from 'axios';
 import QuoteSimpson from './components/QuoteSimpson';
 
-const quoteIni = [{
+const quoteIni = {
   quote: 'Ahh! Sweet liquor eases the pain.',
   character: 'Troy McClure',
   image:'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FTroyMcClure.png?1497567511112',
-}]
+}
 
 
 class App extends React.Component {
   constructor(props){
     super(props);
-    this.state = [{
-        quoteAlea: quoteIni
-      }];  
-    
+    this.state = {
+      quote: quoteIni.quote,
+      character:quoteIni.character,
+      image:quoteIni.image
+    };  
     this.getQuote = this.getQuote.bind(this);
   }
 
@@ -27,16 +28,22 @@ class App extends React.Component {
       .then(response => response.data)
       // Use this data to update the state
       .then(data => {
+        console.log(data);
         this.setState({
-          quoteAlea: data.results
+          quote: data[0].quote,
+          character: data[0].character,
+          image: data[0].image
         });
+        console.log(this.state);
     });
   }
+
+ 
 
     render() {
     return (
       <div className="App">
-        <QuoteSimpson quoteAlea={this.state.quoteAlea[0]} />
+       <QuoteSimpson {...this.state} />
         <button type="button" onClick={this.getQuote}>Get quote</button>
       </div>
     );
